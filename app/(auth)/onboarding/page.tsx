b545355288de7b6c0facdbffef02892f1d3fcb9b
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-08-08 22:45:26 +0100

    Update page.tsx

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import AccountProfile from '@/components/forms/AccountProfile';
-import { currentUser } from '@clerk/nextjs';
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-  // Assuming `user` is an object with the following properties: id, firstName, username, imageUrl, etc.
-  // If `user` and `userInfo` are the same, you don't need two separate variables.
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
+
   const userData = {
-    id: user?.id,
-    objectId: user?._id,
-    username: user?.username,
-    name: user?.firstName ?? '',
-    bio: user?.bio || '',
-    image: user?.imageUrl || '',
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
   };
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
       <p className='mt-3 text-base-regular text-light-2'>
-        Complete your profile now to use Threds.
+        Complete your profile now, to use Threds.
       </p>
 
       <section className='mt-9 bg-dark-2 p-10'>
