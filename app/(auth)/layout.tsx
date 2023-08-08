8e81f066d6dbc1749542c966b969bfdb4c53d82b
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-08-08 23:00:17 +0100

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+const YOUR_CLERK_FRONTEND_API_KEY = 'pk_test_cmVsaWV2ZWQtc2FpbGZpc2gtMTMuY2xlcmsuYWNjb3VudHMuZGV2JA'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider frontendApi={YOUR_CLERK_FRONTEND_API_KEY}>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
